cb35a01c217007b2f3362b3d03230612bce5a266
Jeison Araya Mena <57369279+jeison-araya@users.noreply.github.com>
2023-03-16 19:17:21 -0600

    Base project created

---
+++ app/src/main/java/ucr/ac/cr/ingenieria/App.java
@@ -0,0 +1,13 @@
+package ucr.ac.cr.ingenieria;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
